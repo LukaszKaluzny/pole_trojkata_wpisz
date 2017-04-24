fb625e3fdee2239950e1fda80599f62f0a6f7080
Lukasz Szczesniak <veiner87@gmail.com>
2017-04-24 15:21:04 +0200

    added files

---
+++ script.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj podstawę trójkąta'),
+	h = prompt ('Podaj wysokość trójkąta'),
+	triangleArea = a*h/2;
+
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
